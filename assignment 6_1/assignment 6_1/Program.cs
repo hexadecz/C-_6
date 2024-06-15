1ac57a0ee404f3385196ad6fc4065d2fd343a49a
Ziad Ahmed <161942432+hexadecz@users.noreply.github.com>
2024-06-15 15:24:22 +0300

    Add files via upload

---
+++ assignment 6_1/assignment 6_1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_6_1
+{
+
+    class point
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            set { x = value; }
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public point()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        { return base.ToString() + $"\nX-Coordinates={X}\tY-coordinates{Y}"; }
+    }
+
+    class circle : point
+    {
+        public int radius;
+
+        public int Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public circle()
+        {
+            Radius = 0;
+        }
+        public circle(int x, int y, int radius) : base(x, y)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public override string ToString()
+        { return base.ToString() + $"Radius:{Radius}"; }
+    }
+  internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+  
+
+
+  
---
+++ assignment 6_2/assignment 6_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_6_2
+{
+    class person
+    {
+        string ID;
+        string name;
+        int age;
+        string gender;
+
+        public override string ToString()
+        {
+            return $"person: {name}({age} years old ,{gender})";
+        }
+
+        public virtual double CalculateTax()
+        {
+            return 0;
+        }
+    }
+    class employee: person
+    {
+        protected double salary;
+
+        public override double CalculateTax()
+        {
+            return salary * 0.1;
+        }
+    }
+
+    class SalesMan :employee
+    {
+        double commission;
+        public override double CalculateTax()
+        {
+            return salary * 0.15 + commission*0.1 ;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
